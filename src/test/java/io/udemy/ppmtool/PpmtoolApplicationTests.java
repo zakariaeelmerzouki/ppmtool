c12382aacf968bc897a3aa3dc06626aae814725b
zak <zakaria_el_merzouki@carrefour.com>
2019-10-24 17:29:31 +0200

    update

--- src/test/java/io/udemy/ppmtool/PpmtoolApplicationTests.java
+++ src/test/java/io/udemy/ppmtool/PpmtoolApplicationTests.java
@@ -1,13 +1,8 @@
 package io.udemy.ppmtool;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PpmtoolApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
